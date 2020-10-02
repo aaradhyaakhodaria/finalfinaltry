50caa5f803ddd0a5198895431f46cb3122cc2a16
aaradhyaakhodaria <72060083+aaradhyaakhodaria@users.noreply.github.com>
2020-10-02 12:23:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
-customhost = "student-101.c5ylf3yirenb.us-east-2.rds.amazonaws.com"
+customhost = "student.c5ylf3yirenb.us-east-2.rds.amazonaws.com"
 customuser = "aarmanvu"
 custompass = "thinkbig1995"
-customdb = "student-101"
+customdb = "student"
 custombucket = "addstudent101"
 customregion = "us-east-2"
